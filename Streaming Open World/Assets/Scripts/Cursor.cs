8dad67e34551f3d2f4943c882e61fad532cc8a76
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-02-22 14:49:37 +0000

    3rd person character controller added

---
+++ Streaming Open World/Assets/Scripts/Cursor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private void Start()
+    {
+        UnityEngine.Cursor.visible = false;
+    }
+}
+
