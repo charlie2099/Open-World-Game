361c1232a77b817491f182f74f979d04df1e74d0
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-02-23 22:10:06 +0000

    Chunks now unload without error | Terrain chunks now position correctly

--- Streaming Open World/Assets/Scripts/Chunk.cs
+++ Streaming Open World/Assets/Scripts/Chunk.cs
@@ -1,50 +1,70 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
     [SerializeField] private List<TerrainData> terrainDataList = new List<TerrainData>();
+    
     private List<GameObject> chunksList = new List<GameObject>();
-    private GameObject chunk;
-    private bool[] isLoaded;
+    private GameObject _chunk;
 
     private void Start()
     {
-        isLoaded = new bool[terrainDataList.Count];
+        LoadChunk();
     }
 
-    public void LoadChunk()
+    private void LoadChunk()
     {
+        float x = 0;
+        float z = 0;
+        float rowLength = 16;
+        const float spacing = 112.5f;
+        
         if (chunksList.Count < terrainDataList.Count)
         {
-            /*foreach (TerrainData data in terrainDataList)
+            foreach (var tData in terrainDataList)
             {
-                var terrainData = Resources.Load<TerrainData>("Terrain/" + data.name);
-                chunk = Terrain.CreateTerrainGameObject(terrainData);
-                chunksList.Add(chunk);
-                isLoaded[] = true;
-            }*/
-
-            for (var i = 0; i < terrainDataList.Count; i++)
-            {
-                var terrainData = Resources.Load<TerrainData>("Terrain/" + terrainDataList[i].name);
-                chunk = Terrain.CreateTerrainGameObject(terrainData);
-                chunksList.Add(chunk);
-                isLoaded[i] = true;
+                var terrainData = Resources.Load<TerrainData>("Terrain/" + tData.name);
+                _chunk = Terrain.CreateTerrainGameObject(terrainData);
+                _chunk.transform.position = new Vector3(x * spacing, 0, z * spacing);
+                z++;
+                if (z >= rowLength)
+                {
+                    z = 0;
+                    x++;
+                }
+                chunksList.Add(_chunk);
             }
+        }
+
+        if (chunksList.Count == terrainDataList.Count)
+        {
+            //Debug.Log("Chunks: " + chunksList.Count);
+            //Debug.Log("TerrainData: " + terrainDataList.Count);
+            Debug.Log("<color=lime> All Chunks successfully loaded! </color>");
+        }
+        else
+        {
+            Debug.LogError("<color=red> Some Chunks failed to load! </color>");
         }
     }
 
-    public void UnloadChunk()
+    private void UnloadChunk()
     {
-        foreach (var chunk in chunksList)
+        foreach (GameObject chunk in chunksList.ToList())
         {
             chunksList.Remove(chunk);
             Destroy(chunk);
         }
         Resources.UnloadUnusedAssets();
+
+        if (chunksList.Count <= 0)
+        {
+            Debug.Log("<color=orange> All Chunks successfully unloaded! </color>");
+        }
     }
 
     private void Update()
