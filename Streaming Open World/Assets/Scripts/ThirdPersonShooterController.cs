2b528e1a92e3f011d2e4dd0333ddb59dde92afc8
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-03-24 18:47:00 +0000

    Begun implementing a new player controller

---
+++ Streaming Open World/Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Chilli.Ai.Zombies;
+using Cinemachine;
+using StarterAssets;
+using UnityEngine;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask;
+    [SerializeField] private Transform debugTransform;
+    [SerializeField] private Transform vfxHitEffect;
+
+    private ThirdPersonController thirdPersonController;
+    private StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+        thirdPersonController = GetComponent<ThirdPersonController>();
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        Transform hitTransform = null;
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+            hitTransform = raycastHit.transform;
+        }
+        
+        if (starterAssetsInputs.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
+            thirdPersonController.SetRotateOnMove(false);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20.0f);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
+            thirdPersonController.SetRotateOnMove(true);
+        }
+
+        if (starterAssetsInputs.shoot)
+        {
+            if (hitTransform != null)
+            {
+                if (hitTransform.GetComponent<Zombie>() != null)
+                {
+                    // zombie hit effect + damage
+                }
+                else
+                {
+                    // didn't hit zombie
+                }
+
+                Instantiate(vfxHitEffect, mouseWorldPosition, Quaternion.identity);
+                // muzzle flash effect 
+            }
+        }
+    }
+}
